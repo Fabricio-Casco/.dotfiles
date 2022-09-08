f4269885edb02a883dc60af5ddde96110829266d
nicolas casco <ncasco@DESKTOP-NPU07KI.localdomain>
2022-09-08 01:53:32 -0300

    Se eliminao una instalacion de sumneko lua (estaba duplicado) y se agrega la opcion de hacer packersync sin entrar a vim

--- config/nvim/plugin/packer_compiled.lua
+++ config/nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/fcasco/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/fcasco/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/fcasco/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/fcasco/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/fcasco/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/ncasco/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/ncasco/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/ncasco/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/ncasco/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/ncasco/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,122 +76,127 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
+  },
+  ["auto-pairs"] = {
+    loaded = true,
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/auto-pairs",
+    url = "https://github.com/jiangmiao/auto-pairs"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   gruvbox = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/gruvbox",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/gruvbox",
     url = "https://github.com/gruvbox-community/gruvbox"
   },
   harpoon = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/harpoon",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/ThePrimeagen/harpoon"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/kyazdani42/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["oceanic-next"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/oceanic-next",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/oceanic-next",
     url = "https://github.com/mhartington/oceanic-next"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   playground = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/playground",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/playground",
     url = "https://github.com/nvim-treesitter/playground"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["popup.nvim"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/popup.nvim",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/popup.nvim",
     url = "https://github.com/nvim-lua/popup.nvim"
   },
   ["rust-analyzer"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/rust-analyzer",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/rust-analyzer",
     url = "https://github.com/rust-lang/rust-analyzer"
   },
   ["telescope-fzy-native.nvim"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/telescope-fzy-native.nvim",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/telescope-fzy-native.nvim",
     url = "https://github.com/nvim-telescope/telescope-fzy-native.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["tokyonight.nvim"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
   },
   undotree = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/undotree",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
   ["vim-be-good"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/vim-be-good",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/vim-be-good",
     url = "https://github.com/ThePrimeagen/vim-be-good"
   },
   ["vim-wakatime"] = {
     loaded = true,
-    path = "/home/fcasco/.local/share/nvim/site/pack/packer/start/vim-wakatime",
+    path = "/home/ncasco/.local/share/nvim/site/pack/packer/start/vim-wakatime",
     url = "https://github.com/wakatime/vim-wakatime"
   }
 }
